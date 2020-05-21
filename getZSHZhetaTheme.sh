92e19f8154a4063e64e35c7e17726f630758c22e
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2020-05-22 00:59:56 +0200

    ZSH no longer used

--- getZSHZhetaTheme.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-curl https://raw.githubusercontent.com/skylerlee/zeta-zsh-theme/master/zeta.zsh-theme > $DIR/oh-my-zsh/themes/zeta.zsh-theme
